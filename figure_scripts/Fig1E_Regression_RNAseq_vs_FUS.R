b3821b1f356cdf166e71fc6a98581933dc9747d8
Vincent Plagnol <vincent.plagnol@inivata.com>
2015-01-07 09:54:04 +0000

    Added scripts from Chris

--- figure_scripts/Fig1E_Regression_RNAseq_vs_FUS.R
+++ figure_scripts/Fig1E_Regression_RNAseq_vs_FUS.R
@@ -1,31 +1,32 @@
-install.packages("ggplot2")
-
+library(gdata)
 library(ggplot2)
+ 
+RNASeq_vs_FUS <- read.xls("data//Ule_EDtable1.xlsx", sheet=2, method=("csv"), header=TRUE, pattern="Chromosome")
+
 
 # Processing of Ule_EDtable1 sheet 2
- 
+
 # Column names within script differ from those within Ule_EDtable1 due to stylistic changes to table during manuscript preparation 
 
-RNASeq_vs_FUS = read.csv("~/RNASeq_vs_FUS.csv", sep=",", head=TRUE)
-RNASeq_vs_FUS$ratio.after.before.R <- (as.numeric(RNASeq_vs_FUS$ratio.after.before.R))
-RNASeq_vs_FUS$ratio.after.before.F <- (as.numeric(RNASeq_vs_FUS$ratio.after.before.F))
-RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.na(RNASeq_vs_FUS$ratio.after.before.R), ]
-RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.na(RNASeq_vs_FUS$ratio.after.before.F), ]
-RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.infinite(RNASeq_vs_FUS), ]
-RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.infinite(RNASeq_vs_FUS$ratio.after.before.R), ]
+RNASeq_vs_FUS$Ratio.after.before_R <- (as.numeric(RNASeq_vs_FUS$Ratio.after.before_R))
+RNASeq_vs_FUS$Ratio.after.before_F <- (as.numeric(RNASeq_vs_FUS$Ratio.after.before_F))
+RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.na(RNASeq_vs_FUS$Ratio.after.before_R), ]
+RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.na(RNASeq_vs_FUS$Ratio.after.before_F), ]
+RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.infinite(RNASeq_vs_FUS$Ratio.after.before_R), ]
+RNASeq_vs_FUS <- RNASeq_vs_FUS[!is.infinite(RNASeq_vs_FUS$Ratio.after.before_F), ]
 
 #Select out non-sig
-RNASeq_vs_FUS_non_sig <- subset(RNASeq_vs_FUS, sig == "FALSE")
+RNASeq_vs_FUS_non_sig <- subset(RNASeq_vs_FUS, Double.Significance.and.improved.gradient == "NO")
 
 #Select out sig unproductive going the right way
-RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS, ratio.after.before.R >"1")
-RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS_sig, ratio.after.before.F >"1")
-RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS_sig, sig == "SIG")
+RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS, Ratio.after.before_R >"1")
+RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS_sig, Ratio.after.before_F >"1")
+RNASeq_vs_FUS_sig <- subset(RNASeq_vs_FUS_sig, Double.Significance.and.improved.gradient == "YES")
 
 #select out sig unproductive going the wrong way and bind to non-sig#
-RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS_2, ratio.after.before.R <="0")
-RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS_wrong_way, ratio.after.before.F <="0")
-RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS_wrong_way, sig == "SIG")
+RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS, Ratio.after.before_R <="0")
+RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS_wrong_way, Ratio.after.before_F <="0")
+RNASeq_vs_FUS_wrong_way <- subset(RNASeq_vs_FUS_wrong_way, Double.Significance.and.improved.gradient == "YES")
 RNASeq_vs_FUS_non_sig_wrong_way <- rbind(RNASeq_vs_FUS_non_sig, RNASeq_vs_FUS_wrong_way)
 
 #select out recursive sites
@@ -40,5 +41,7 @@ RNASeq_vs_FUS_RSS$Extra <- "Motif B"
 
 RNASeq_vs_FUS_all <- rbind(RNASeq_vs_FUS_non_sig_wrong_way, RNASeq_vs_FUS_sig, RNASeq_vs_FUS_RSS)
 
-ggplot(RNASeq_vs_FUS_all) + geom_point(aes(x = log10(ratio.after.before.R), y = log10(ratio.after.before.F), color=factor(Extra), size=2)) + scale_colour_manual(values=c("black", "red", "grey")) + geom_hline(y=log10(0.894654152), colour="steelblue", linetype = "longdash") + geom_hline(y=log10(1.284250294), colour="steelblue", linetype = "longdash") + geom_vline(x=log10(0.885466603), colour="steelblue", linetype = "longdash") + geom_vline(x=log10(1.198153005), colour="steelblue", linetype = "longdash") + ylim(c(-1.5, 1.5)) + xlim(c(-1.5, 1.5))
+p <- ggplot(RNASeq_vs_FUS_all) + geom_point(aes(x = log10(Ratio.after.before_R), y = log10(Ratio.after.before_F), color=factor(Extra), size=2)) + scale_colour_manual(values=c("black", "red", "grey")) + geom_hline(y=log10(0.894654152), colour="steelblue", linetype = "longdash") + geom_hline(y=log10(1.284250294), colour="steelblue", linetype = "longdash") + geom_vline(x=log10(0.885466603), colour="steelblue", linetype = "longdash") + geom_vline(x=log10(1.198153005), colour="steelblue", linetype = "longdash") + ylim(c(-1.5, 1.5)) + xlim(c(-1.5, 1.5))
 
+
+ggsave(plot = p, filename = 'fig/Fig1E_regression_RNASeq_vs_FUS.pdf')
--- figure_scripts/Fig4a_First_intron_novel_junctions.R
+++ figure_scripts/Fig4a_First_intron_novel_junctions.R
@@ -1,12 +1,11 @@
-install.packages("ggplot2")
-install.packages("plyr")
-
+library(gdata)
 library(ggplot2)
 library(plyr)
 
-# Restrict analysis only to those genes were canonical first exon-exon junction detected
+ 
+Cryptic <- read.xls("data//Ule_EDtable1.xlsx", sheet=5, method=("csv"), header=TRUE, pattern="Chromosome")
 
-Cryptic = read.csv("~/Cryptic_first.csv", sep=",", head=TRUE)
+# Restrict analysis only to those genes were canonical first exon-exon junction detected
 
 Keep <- Cryptic[grep("Exon-Exon", Cryptic$Junction.type), ]
 Keep <- subset(Keep, Counts >= "1")
@@ -17,7 +16,10 @@ Cryptic <- Cryptic[which(Cryptic$Gene.ID %in% Keep$Gene.ID),]
 Cryptic_summary <- ddply(Cryptic, c("Gene.ID", "X1st.Intron.Bin"), summarise, Novel=sum(Junction.type=="Novel"))
 Cryptic_summary$X1st.Intron.Bin <- factor(Cryptic_summary$X1st.Intron.Bin, levels = c("0.1-2kb", "2-5kb", "5-10kb", "10-20kb", "20-50kb", "50-100kb", "100kb+"))
 
-ggplot(Cryptic_summary) + geom_boxplot(aes(x=X1st.Intron.Bin, y=Novel), outlier.size = 0, fill="#0072B2") + coord_cartesian(ylim = c(-0.2, 10)) +  scale_y_continuous(breaks=seq(0, 10, 2)) + stat_summary(aes(x=X1st.Intron.Bin, y=Novel), fun.y = "mean", geom = "point",  pch= 18, size= 7, color= "red") 
+p <- ggplot(Cryptic_summary) + geom_boxplot(aes(x=X1st.Intron.Bin, y=Novel), outlier.size = 0, fill="#0072B2") + coord_cartesian(ylim = c(-0.2, 10)) +  scale_y_continuous(breaks=seq(0, 10, 2)) + stat_summary(aes(x=X1st.Intron.Bin, y=Novel), fun.y = "mean", geom = "point",  pch= 18, size= 7, color= "red") 
+
+ggsave(plot = p, filename = 'fig/Fig4A_first_intron_novel_junction.pdf')
+
 
 # Mann Whitney U test with two tails / Wilcoxon Rank Sum Test
 
