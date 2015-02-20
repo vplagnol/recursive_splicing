9d895111f28d1929684325a9864b4bd004898982
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-02-20 06:14:38 +0000

    Minor update

--- figure_scripts/FigED6_FUS_mouse_seesaw.R
+++ figure_scripts/FigED6_FUS_mouse_seesaw.R
@@ -2,8 +2,9 @@
 
 library(GenomicRanges)
 
-count.data <- read.table("data/group_3094_FUS-E18-all_sum_G_mm9--ensembl59_from_2320-3089-3090_bedGraph-cDNA-hits-in-genome.bed.gz", header = FALSE, sep = "\t", skip = 1)
+#count.data <- read.table("data/group_3094_FUS-E18-all_sum_G_mm9--ensembl59_from_2320-3089-3090_bedGraph-cDNA-hits-in-genome.bed.gz", header = FALSE, sep = "\t", skip = 1)
 
+intron.positions <- read.csv("data/Mouse_RSS_introns.csv", header = FALSE, stringsAsFactors = FALSE)
 mouse.RSS <- read.csv("data/Mouse_RSS_mm9.csv", header = FALSE, stringsAsFactors = FALSE)
 
 
@@ -13,13 +14,18 @@ par(mfrow = c(4, 2))
 n.sites <- nrow(mouse.RSS)
 region.halfwidth <- 200*1000
 
+pval.tables <- data.frame(clean.names = rep(NA_character_, 8), ANOVA.pvals = NA, stringsAsFactors = FALSE)
+
 for (i in 1:8) {
   message("Plotting RS ", i)
   chromosome <- mouse.RSS$V2[ i ]
   left <- mouse.RSS$V3[ i ] - region.halfwidth
   right <- mouse.RSS$V3[ i ] + region.halfwidth
+  RSS.name <- gsub(pattern = "_1$", replacement = "", mouse.RSS$V1[ i ])
 
-  if (mouse.RSS$V1[ i ] == "PDE4D_1") {left <- 110123705 - region.halfwidth; right <- 110404615 + region.halfwidth}
+  left <- intron.positions$V3[ match(tolower(RSS.name), tolower(intron.positions$V1)) ]
+  right <- intron.positions$V4[ match(tolower(RSS.name), tolower(intron.positions$V1)) ]
+  clean.name <- intron.positions$V1[ match(tolower(RSS.name), tolower(intron.positions$V1)) ]
   
   count.data.loc <- subset(count.data, V1 == chromosome & V2 >= left & V2 <= right)
   G.count.data.loc <- GRanges (seqnames = count.data.loc$V1, IRanges( count.data.loc$V2, count.data.loc$V3)) 
@@ -35,24 +41,26 @@ for (i in 1:8) {
   for.regression <- data.frame(x = 0.5*(start(my.grid) + end(my.grid)), y = ifelse ( is.na(combined.sum), 0, combined.sum) )  ##check the NA issue, looks suspicious
   for.regression$RSS <- for.regression$x > mouse.RSS$V3[ i ] 
 
-  if (mouse.RSS$V1[ i ] == "PDE4D_1") {
-    for.regression$sec.RSS <- for.regression$x > mouse.RSS$V3[ 9 ]
-  }
+  if (RSS.name == "PDE4D") for.regression$sec.RSS <- for.regression$x > mouse.RSS$V3[ 9 ]
+  if (RSS.name == "CADM2") for.regression$sec.RSS <- for.regression$x > 67143137  ### adds the second RSS site in CADM2
   
   artificial.reads <- rep(x = for.regression$x / 1000, times = for.regression$y) ## this is a bit of a hack but well... I want a histogram
   my.hist <- hist( artificial.reads, breaks = seq(from = left, to = right , by = 5000) / 1000, plot = FALSE)
   
-
   for.regression$xclean <- my.hist$mids - min(my.hist$mids)
-
-  ####### exclude the region outside of genes
-  for.regression$exclude <- cumsum(for.regression$y) - max(cumsum(for.regression$y)) >= -10 | cumsum(for.regression$y) < 10
-  for.regression <- subset(for.regression, ! exclude )
+  
+  ####### exclude the region outside of genes- no need anymore because I use proper intron definitions
+  #for.regression$exclude <- cumsum(for.regression$y) - max(cumsum(for.regression$y)) >= -10 | cumsum(for.regression$y) < 10
+  #for.regression <- subset(for.regression, ! exclude )
   
   basic.model <- lm (for.regression, formula = " y ~ xclean")
   with.RSS <- lm (for.regression, formula = " y ~ xclean + RSS")
 
-  if (mouse.RSS$V1[ i ] == "PDE4D_1") {  with.RSS <- lm (for.regression, formula = " y ~ xclean + RSS + sec.RSS" )}
+  print(anova(basic.model, with.RSS))
+  pval.tables$clean.names[i]  <- clean.name
+  pval.tables$ANOVA.pvals[i]  <- anova(basic.model, with.RSS)[2,6]
+  
+  if (RSS.name %in% c("PDE4D", "CADM2")) {  with.RSS <- lm (for.regression, formula = " y ~ xclean + RSS + sec.RSS" )}
   
   fitted.ordered <- fitted(basic.model)
   fitted.with.RSS <- fitted( with.RSS )
@@ -62,16 +70,17 @@ for (i in 1:8) {
   plot(my.hist,
        xlab = 'Position (kb)',
        ylab = 'Read count',
-       xlim = range(for.regression$x)/1000,
+       xlim = c(left, right)/1000,
        col = 'red',
-       main = paste0(label[ i ], ". ", gsub(pattern = "_1$", replacement = "", mouse.RSS$V1[ i ])))
+       main = paste0(label[ i ], ". ", clean.name))
   
   points ( x = for.regression$x/ 1000, y = fitted.ordered, type = 'l', lty = 1, col = 'black', lwd = 2)
   points ( x = for.regression$x/ 1000, y = fitted.with.RSS, type = 'l', lty = 1, col = 'blue', lwd = 2)
   abline ( v = mouse.RSS$V3[ i ]/ 1000, col = "blue")
 
 
-  if (mouse.RSS$V1[ i ] == "PDE4D_1") {  abline ( v = mouse.RSS$V3[ 9 ]/ 1000, col = "blue")}
+  if (RSS.name == "PDE4D") {  abline ( v = mouse.RSS$V3[ 9 ]/ 1000, col = "blue")}
+  if (RSS.name == "CADM2") {  abline ( v = 67143137/1000, col = "blue")}
 }
 
 
